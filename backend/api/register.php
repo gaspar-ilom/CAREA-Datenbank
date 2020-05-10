3a0ad2e910322733bbe2621c7889237883c04549
clandestino <clandestino@warumnur.org>
2020-05-11 00:43:34 +0200

    add register.php

---
+++ backend/api/register.php
@@ -0,0 +1,128 @@
+<?php
+header('Content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+  exit;
+}
+
+// Check if the user is logged in
+require_once '../config_jwt.php';
+if (!isAuthorized()) {
+  http_response_code(401);
+  echo '[]';
+  exit;
+}
+
+// Include config file
+require_once "../config.php";
+
+// Define variables and initialize with empty values
+$username     = $password     = $confirm_password     = "";
+$username_err = $password_err = $confirm_password_err = "";
+$err = "";
+$success      = false;
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  // Validate username
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter a username.";
+  } else {
+    // Prepare a select statement
+    $sql = "SELECT id FROM users WHERE username = ?";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+      // Set parameters
+      $param_username = trim($_POST["username"]);
+
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        /* store result */
+        mysqli_stmt_store_result($stmt);
+
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+          $username_err = "This username is already taken.";
+        } else {
+          $username = trim($_POST["username"]);
+        }
+      } else {
+        $err = "Oops! Something went wrong. Please try again later.";
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
+    }
+  }
+
+  // Validate password
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter a password.";
+  } elseif (strlen(trim($_POST["password"])) < 8) {
+    $password_err = "Password must have at least 8 characters.";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  // Validate confirm password
+  if (empty(trim($_POST["confirm_password"]))) {
+    $confirm_password_err = "Please confirm password.";
+  } else {
+    $confirm_password = trim($_POST["confirm_password"]);
+    if (empty($password_err) && ($password != $confirm_password)) {
+      $confirm_password_err = "Password did not match.";
+    }
+  }
+
+  // Check input errors before inserting in database
+  if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
+    // Prepare an insert statement
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+      // Set parameters
+      $param_username = $username;
+      $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        // Redirect to login page
+        $success = true;
+      } else {
+        $err = "Something went wrong. Please try again later.";
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
+    }
+  }
+
+  // Close connection
+  mysqli_close($link);
+
+}
+
+if ($success) {
+  http_response_code(201);
+  echo json_encode(array("message" => "User succesfully created."));
+  exit;
+}
+http_response_code(400);
+echo json_encode(array(
+  "message" => "User not created.",
+  "username" => $username_err,
+  "password" => $password_err,
+  "confirm_password" => $confirm_password_err,
+  "error" => $err
+));
